4f48d2f51adb822b26279440c9839fc8338573d5
Michał Bartkiewicz <michal4421@outlook.com>
2016-08-03 21:06:12 +0200

    Dodanie projektu do repozytorium

---
+++ Source/Lublin/LublinGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "LublinGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LUBLIN_API ALublinGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
