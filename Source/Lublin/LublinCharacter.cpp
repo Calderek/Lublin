30e0f8cb315cc713c42da43908b67c5ef907c3f7
Calderek <michal4421@outlook.com>
2017-05-05 14:44:29 +0200

    Dodanie kucania, skakania, zooma

--- Source/Lublin/LublinCharacter.cpp
+++ Source/Lublin/LublinCharacter.cpp
@@ -10,21 +10,25 @@ ALublinCharacter::ALublinCharacter()
 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	//Camera
-	FPSCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("FirstPersonCamera"));
-	FPSCameraComponent->AttachTo(GetCapsuleComponent());
-	FPSCameraComponent->SetRelativeLocation(FVector(25.0f, 1.0f, 74.0f));
-	FPSCameraComponent->bUsePawnControlRotation = true;
+	//Camera	
+	OurCameraSpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraSpringArm"));
+	OurCameraSpringArm->SetupAttachment(GetCapsuleComponent());
+	OurCameraSpringArm->SetRelativeLocationAndRotation(FVector(0.0f, 0.0f, 50.0f), FRotator(-60.0f, 0.0f, 0.0f));
+	OurCameraSpringArm->TargetArmLength = 400.f;
+	OurCameraSpringArm->bEnableCameraLag = true;
+	OurCameraSpringArm->CameraLagSpeed = 3.0f;
+	OurCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("GameCamera"));
+	OurCamera->SetupAttachment(OurCameraSpringArm, USpringArmComponent::SocketName);
 
-	//this->GetCharacterMovement()->JumpZVelocity = 600.0f;
-	//this->GetCharacterMovement()->AirControl = 0.5f;
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
 }
 
 // Called when the game starts or when spawned
 void ALublinCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Yellow, TEXT("using LublinCharacter"));
+
 }
 
 // Called every frame
@@ -32,6 +36,27 @@ void ALublinCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	{
+		if (bZoomingIn)
+		{
+			ZoomFactor += DeltaTime / 0.5f;         //Zoom in over half a second
+		}
+		else
+		{
+			ZoomFactor -= DeltaTime / 0.25f;        //Zoom out over a quarter of a second
+		}
+		ZoomFactor = FMath::Clamp<float>(ZoomFactor, 0.0f, 1.0f);
+		//Blend our camera's FOV and our SpringArm's length based on ZoomFactor
+		OurCamera->FieldOfView = FMath::Lerp<float>(90.0f, 60.0f, ZoomFactor);
+		OurCameraSpringArm->TargetArmLength = FMath::Lerp<float>(400.0f, 300.0f, ZoomFactor);
+	}
+
+	//Rotate our camera's pitch, but limit it so we're always looking downward
+	{
+		FRotator NewRotation = OurCameraSpringArm->GetComponentRotation();
+		NewRotation.Pitch = FMath::Clamp(NewRotation.Pitch + CameraInput.Y, -80.0f, -15.0f);
+		OurCameraSpringArm->SetWorldRotation(NewRotation);
+	}
 }
 
 // Called to bind functionality to input
@@ -42,31 +67,69 @@ void ALublinCharacter::SetupPlayerInputComponent(class UInputComponent* InputCom
 	// Set up "movement" bindings.
 	InputComponent->BindAxis("MoveForward", this, &ALublinCharacter::MoveForward);
 	InputComponent->BindAxis("MoveRight", this, &ALublinCharacter::MoveRight);
-	InputComponent->BindAxis("Turn", this, &ALublinCharacter::AddControllerYawInput);
-	InputComponent->BindAxis("LookUpDown", this, &ALublinCharacter::AddControllerPitchInput);
+	InputComponent->BindAxis("CameraPitch", this, &ALublinCharacter::PitchCamera);
+	InputComponent->BindAxis("CameraYaw", this, &ALublinCharacter::AddControllerYawInput);
 	InputComponent->BindAction("Jump", IE_Pressed, this, &ALublinCharacter::StartJump);
 	InputComponent->BindAction("Jump", IE_Released, this, &ALublinCharacter::StopJump);
 	InputComponent->BindAction("Quit", IE_Pressed, this, &ALublinCharacter::QuitLublin);
+	InputComponent->BindAction("Crouch", IE_Pressed, this, &ALublinCharacter::OnCrouch);
+	InputComponent->BindAction("Crouch", IE_Released, this, &ALublinCharacter::OnCrouchEnd);
+	InputComponent->BindAction("ZoomIn", IE_Pressed, this, &ALublinCharacter::ZoomIn);
+	InputComponent->BindAction("ZoomIn", IE_Released, this, &ALublinCharacter::ZoomOut);
 }
 
-void ALublinCharacter::MoveForward(float Value)
+void ALublinCharacter::MoveForward(float AxisValue)
 {
 	FVector Direction = FRotationMatrix(Controller->GetControlRotation()).GetScaledAxis(EAxis::X);
-	AddMovementInput(Direction, Value);
+	AddMovementInput(Direction, AxisValue);
 }
 
-void ALublinCharacter::MoveRight(float Value)
+void ALublinCharacter::MoveRight(float AxisValue)
 {
 	FVector Direction = FRotationMatrix(Controller->GetControlRotation()).GetScaledAxis(EAxis::Y);
-	AddMovementInput(Direction, Value);
+	AddMovementInput(Direction, AxisValue);
 }
 
 void ALublinCharacter::StartJump()
 {
-
-	Jump();
-
+	if (CanJumpInternal()) {
+		JumpButtonDown = true;
+	}
+	UCharacterMovementComponent &mov = *Cast <UCharacterMovementComponent>(GetMovementComponent());
+	if (mov.Velocity.X || mov.Velocity.Y)
+		Jump();
 }
+
+void ALublinCharacter::OnCrouch()
+{
+	CrouchButtonDown = true;
+}
+
+void ALublinCharacter::OnCrouchEnd()
+{
+	CrouchButtonDown = false;
+}
+
+void ALublinCharacter::PitchCamera(float AxisValue)
+{
+	CameraInput.Y = AxisValue;
+}
+
+void ALublinCharacter::YawCamera(float AxisValue)
+{
+	CameraInput.X = AxisValue;
+}
+
+void ALublinCharacter::ZoomIn()
+{
+	bZoomingIn = true;
+}
+
+void ALublinCharacter::ZoomOut()
+{
+	bZoomingIn = false;
+}
+
 
 void ALublinCharacter::QuitLublin()
 {
@@ -75,6 +138,6 @@ void ALublinCharacter::QuitLublin()
 
 void ALublinCharacter::StopJump()
 {
-	bPressedJump = false;
+	JumpButtonDown = false;
 
 }
--- Source/Lublin/LublinCharacter.h
+++ Source/Lublin/LublinCharacter.h
@@ -16,32 +16,55 @@ public:
 
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-	
+
 	// Called every frame
-	virtual void Tick( float DeltaSeconds ) override;
+	virtual void Tick(float DeltaSeconds) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* InputComponent) override;
 
 	// Handles input for moving forward and backward.
 	UFUNCTION()
-		void MoveForward(float Value);
+		void MoveForward(float AxisValue);
 
 	// Handles input for moving right and left.
 	UFUNCTION()
-		void MoveRight(float Value);
+		void MoveRight(float AxisValue);
 
 	//Handles jumping.
 	UFUNCTION()
 		void StartJump();
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		bool JumpButtonDown;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		bool CrouchButtonDown;
+
 	UFUNCTION()
 		void QuitLublin();
 	UFUNCTION()
 		void StopJump();
+	UFUNCTION()
+		void OnCrouch();
+	UFUNCTION()
+		void OnCrouchEnd();
+	UPROPERTY(EditAnywhere)
+		USpringArmComponent* OurCameraSpringArm;
+	UCameraComponent* OurCamera;
+
+	//Input variables
+	FVector2D MovementInput;
+	FVector2D CameraInput;
+	float ZoomFactor;
+	bool bZoomingIn;
+
+	void PitchCamera(float AxisValue);
+	void YawCamera(float AxisValue);
+	void ZoomIn();
+	void ZoomOut();
 
 	//Camera
 	UPROPERTY(VisibleAnywhere)
 		UCameraComponent* FPSCameraComponent;
-	
+
 };
